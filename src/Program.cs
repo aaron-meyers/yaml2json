b3665eea3249130eb0874db84758b7bd7f6ea880
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2019-06-12 10:18:42 -0700

    Added yaml2json cli

---
+++ src/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CommandLine;
+using Newtonsoft.Json;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using CliParser = CommandLine.Parser;
+using YamlParser = YamlDotNet.Core.Parser;
+
+namespace Yaml2Json
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var result = CliParser.Default.ParseArguments(args, typeof(CliArgs));
+            await result.MapResult<CliArgs, Task>(a => Run(a), e => Task.FromResult(false));
+        }
+
+        private static async Task Run(CliArgs args)
+        {
+            if (string.IsNullOrWhiteSpace(args.Target))
+                args.Target = Path.ChangeExtension(args.Source, ".json");
+
+            using (var reader = new StreamReader(args.Source))
+            using (var writer = new StreamWriter(args.Target))
+            {
+                var deserializer = new DeserializerBuilder().Build();
+                var parser = new YamlParser(reader);
+                parser.Expect<StreamStart>();
+                while (parser.Current is DocumentStart)
+                {
+                    object source = deserializer.Deserialize(parser);
+
+                    if (source != null)
+                        await writer.WriteLineAsync(JsonConvert.SerializeObject(source));
+                }
+                parser.Expect<StreamEnd>();
+            }
+        }
+    }
+
+    [Verb("convert")]
+    public sealed class CliArgs
+    {
+        [Value(0, Required = true)]
+        public string Source { get; set; }
+
+        [Value(1, Required = false)]
+        public string Target { get; set; }
+    }
+}
